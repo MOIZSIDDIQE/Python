04491db90bda7ebb8bf842acd4dd16829f2fcd4e
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-08-22 16:46:55 +0500

    Learn input and dataTypes

--- Practice_work/first.py
+++ Practice_work/first.py
@@ -12,3 +12,11 @@ a *= b
 print(a)
 
 
+name = input("What is your name :")
+print("welcome ",name,"!")
+
+print("Write Numbers to do sum !")
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+print("sum =",num1 + num2)
