2f9229b352d9a9014c736f1a42b9ca080a5ac2e4
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-08-22 18:50:23 +0500

    learn capatalize,find,count and replace function

--- Practice_work/second.py
+++ Practice_work/second.py
@@ -3,3 +3,11 @@
 Name2 = "i am elder the elder brother"
 print(Name2.endswith("er"))
 
+#capatalize function : ya func str ke start ke letter ko capatale kar deta ha or dosre sab ko lowercase.
+# Example:
+Name3 = "i am elder the Elder Brother"
+print(Name3.capitalize())
+print(Name3.find("B")) 
+print(Name3.count("e"))
+print(Name3.replace("Elder","younger"))
+
