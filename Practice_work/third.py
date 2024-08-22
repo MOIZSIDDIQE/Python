9f7fda27e8bfa8b7048d558726730c05dd63352b
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-08-22 18:58:43 +0500

    learn list and Its function

---
+++ Practice_work/third.py
@@ -0,0 +1,24 @@
+list = [4,1,3,2]
+
+list.insert(3,6)
+print(list)
+
+list.append(5)
+print(list)
+
+list.remove(2)
+print(list)
+
+list.pop(3)
+print(list)
+
+list.reverse()
+print(list)
+
+list.sort()
+print(list)
+
+list.sort(reverse=True)
+print(list)
+
+
