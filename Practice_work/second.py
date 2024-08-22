3780dd53d3d1e0d236a68b0ce7b4d18ae0f4d477
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-08-22 18:54:05 +0500

    practice

--- Practice_work/second.py
+++ Practice_work/second.py
@@ -20,3 +20,40 @@ if( age >= 18):
 else:
     print("Not Eligible for Government job")
 
+#                                                       practice         
+N_input = input("Enter Your Name: ")
+print(len(N_input))
+
+#                   practice         
+buy = "I bought a Laptop for only $1000 dollar."
+print(buy.count("f"))
+
+grade= int(input("Enter Marks: "))
+
+if(grade >= 90):
+    print("Grade: A")
+elif(grade >=80 and grade<90):
+    print("Grade: B")
+elif(grade >=70 and grade <80):
+    print("Grade: C")
+elif(grade >=50 and grade<70):
+    print("Grade: D")
+else:
+    print("FAIL")
+
+numInput = int(input("Enter Number: "))
+if(numInput%2 == 0):
+    print("Number is even")
+else:
+    print("Numner is odd")
+
+array = ["Moiz",18,85.86,"Karachi"]
+array[1["M"]] = "y"
+print(array[0])
+
+numberArray= [34,56,55,2,44,98,76,59,60,45,65]
+print(numberArray[:6])
+print(numberArray[4:])
+print(numberArray[-5:-2])
+print(numberArray[-8:])
+
