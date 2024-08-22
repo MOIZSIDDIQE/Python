407e127cc4d29fe98c6ec7fb1edb1cd1d458ba54
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-08-22 18:30:52 +0500

    learn endwith function

---
+++ Practice_work/second.py
@@ -0,0 +1,5 @@
+# endwith function : wo function hota ha jo value hum ()me pass kare ke last ka text  agar ya ho to true otherwise false.
+# Example:
+Name2 = "i am elder the elder brother"
+print(Name2.endswith("er"))
+
