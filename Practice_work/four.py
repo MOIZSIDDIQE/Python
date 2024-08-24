7439e4e66cd23b406e088a49d474ec60c08623ca
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-08-24 19:24:04 +0500

    learn dictionary nesting

--- Practice_work/four.py
+++ Practice_work/four.py
@@ -4,6 +4,18 @@ student={
     "Id":456,
     "age":18,
     "skills":["HTML","CSS","TYPESCRIPT","TAILWINDCSS","NODE.JS","PYTHON"],
+    "Subjests":{
+        "Class9th":{
+            "math":89,
+            "english":92,
+            "total":472
+        },
+        "class10th":{
+            "math":84,
+            "english":86,
+            "total":1152}
+    }
 }
 
 print(student["skills"])
+print(student["Subjests"]["class10th"])
