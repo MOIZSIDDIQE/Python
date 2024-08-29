aee06ec5a2142b7164fa2f0e82ddd374fa8cfe00
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-08-29 13:15:46 +0500

    Learn set in python

--- Practice_work/four.py
+++ Practice_work/four.py
@@ -1,33 +1,41 @@
 
-student={
-    "name":"Moiz",
-    "Id":456,
-    "age":18,
-    "skills":["HTML","CSS","TYPESCRIPT","TAILWINDCSS","NODE.JS","PYTHON"],
-    "Subjects":{
-        "Class9th":{
-            "math":89,
-            "english":92,
-            "total":472
-        },
-        "class10th":{
-            "math":84,
-            "english":86,
-            "total":1152}
-    }
-}
+# student={
+#     "name":"Moiz",
+#     "Id":456,
+#     "age":18,
+#     "skills":["HTML","CSS","TYPESCRIPT","TAILWINDCSS","NODE.JS","PYTHON"],
+#     "Subjects":{
+#         "Class9th":{
+#             "math":89,
+#             "english":92,
+#             "total":472
+#         },
+#         "class10th":{
+#             "math":84,
+#             "english":86,
+#             "total":1152}
+#     }
+# }
 
-print(student["skills"])
+# print(student["skills"])
 
-print(student["Subjects"]["class10th"])
+# print(student["Subjects"]["class10th"])
 
-print(student.keys())
+# print(student.keys())
 
-print(student.values())
+# print(student.values())
 
-print(student.items())
+# print(student.items())
 
-print(student.get("skills"))
+# print(student.get("skills"))
 
-print(student.update({"FName":"Siddiq"} ))
-print(student)
+# student.update({"FName":"Siddiq"})
+# print(student)
+
+num = {1,2,4,9,3,4,5,6,7,8,9,10,"java"}
+
+print(num)
+
+
+
+
