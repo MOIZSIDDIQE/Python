3198bcb60e1e74539759a9caa86bc16e277fbcfa
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-08-29 19:34:14 +0500

    Practice 1 : Ques4

--- Practice_work/five.py
+++ Practice_work/five.py
@@ -1,26 +1,42 @@
-count = 1
+# count = 1
 
-while count <=10:
-    print("hello")
-    print(count)
-    count +=1    
+# while count <=10:
+#     print("hello")
+#     print(count)
+#     count +=1    
 
-# practice 1:
-# No 1:
-i =1
-while i<=100:
-    print(i)
+# # practice 1:
+# # No 1:
+# i =1
+# while i<=100:
+#     print(i)
+#     i +=1
+
+# # No:2
+# i =100
+# while i>=1:
+#     print(i)
+#     i -=1     
+
+# # No:3
+# n = int(input("Which number of table do you want. Enter your number :"))
+# i = 1
+# while i<=10:
+#     print(n*i)
+#     i +=1    
+
+# No4:
+lists=[] 
+
+n = 10
+i = 1
+while i<=n:
+    lists.append(i*i)
     i +=1
 
-# No:2
-i =100
-while i>=1:
-    print(i)
-    i -=1     
+print(lists)
 
-# No:3
-n = int(input("Which number of table do you want. Enter your number :"))
-i = 1
-while i<=10:
-    print(n*i)
+i=0
+while i<len(lists):
+    print(lists[i])
     i +=1
