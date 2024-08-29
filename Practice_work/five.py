6efcf9ac8782e2b515a3105f8fd89f0c8c7b98f4
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-08-29 18:51:03 +0500

    Practice 1 : Ques3

--- Practice_work/five.py
+++ Practice_work/five.py
@@ -12,8 +12,15 @@ while i<=100:
     print(i)
     i +=1
 
-
+# No:2
 i =100
 while i>=1:
     print(i)
-    i -=1 
+    i -=1     
+
+# No:3
+n = int(input("Which number of table do you want. Enter your number :"))
+i = 1
+while i<=10:
+    print(n*i)
+    i +=1
